929dab0db3c5798d4e673b6dc7dd0378c13b9128
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-27 11:47:26 -0500

    install python modules and their requirements

---
+++ scripts/install.sh
@@ -0,0 +1,25 @@
+cd src/grapheme2phoneme
+python setup.py develop
+python -m pip install -r requirements.txt
+cd ..
+
+cd src/music-examples
+python setup.py develop
+python -m pip install -r requirements.txt
+cd ..
+
+cd src/phoneme2grapheme
+python setup.py develop
+python -m pip install -r requirements.txt
+cd ..
+
+cd src/recurrent
+python setup.py develop
+python -m pip install -r requirements.txt
+cd ..
+
+cd src/scrape-lyrics
+python setup.py develop
+python -m pip install -r requirements.txt
+cd ..
+
