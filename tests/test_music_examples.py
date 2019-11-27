33641d1bd5fb00f1cae34ef30004607ccde28a90
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-27 09:59:36 -0500

    create tests for music21 examples unit

---
+++ tests/test_music_examples.py
@@ -0,0 +1,149 @@
+from music_examples import music
+
+
+def test_melody():
+    music.melody()
+
+
+def test_melody_midi():
+    music.melody_midi()
+
+
+def test_metadata():
+    music.metadata()
+
+
+def test_meter():
+    music.meter()
+
+
+def test_midi():
+    music.midi()
+
+
+def test_musedata():
+    music.musedata()
+
+
+def test_musicxml():
+    music.musicxml()
+
+
+def test_note():
+    music.note_example()
+
+
+def test_noteworthy():
+    music.noteworthy()
+
+
+def test_omr():
+    music.omr()
+
+
+def test_patel():
+    music.patel()
+
+
+def test_pitch():
+    music.pitch()
+
+
+def test_repeat():
+    music.repeat()
+
+
+def test_roman():
+    music.roman()
+
+
+def test_romanText():
+    music.romanText()
+
+
+def test_row_mat():
+    music.row_mat()
+
+
+def test_scale():
+    music.scale()
+
+
+def test_search():
+    music.search()
+
+
+def test_serial():
+    music.serial()
+
+
+def test_sieve():
+    music.sieve()
+
+
+def test_sites():
+    music.sites()
+
+
+def test_sorting():
+    music.sorting()
+
+
+def test_spanner():
+    music.spanner()
+
+
+def test_stream():
+    music.stream()
+
+
+def test_style():
+    music.style()
+
+
+def test_tablature():
+    music.tablature()
+
+
+def test_tempo():
+    music.tempo()
+
+
+def test_test():
+    music.test()
+
+
+def test_text():
+    music.text()
+
+
+def test_tie():
+    music.tie()
+
+
+def test_tinyNotation():
+    music.tinyNotation()
+
+
+def test_tree():
+    music.tree()
+
+
+def test_variant():
+    music.variant()
+
+
+def test_vexflow():
+    music.vexflow()
+
+
+def test_voiceLeading():
+    music.voiceLeading()
+
+
+def test_volpiano():
+    music.volpiano()
+
+
+def test_volume():
+    music.volume()
