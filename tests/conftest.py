49184dfa3642cebb6e89331340d66c5ffc88b9e3
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-28 14:11:18 -0500

    setup logger during conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ dirname = os.path.dirname(__file__)
 parent_dirname = os.path.join(dirname, os.pardir)
 dictConfig(config.logging_config_dict)
 logger = logging.getLogger()
+logging.info("conftest")
 
 
 @pytest.fixture
