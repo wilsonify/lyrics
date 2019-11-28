0ae6a00eff9298be11adf93ac4b1cec9d71dc50f
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-28 14:05:11 -0500

    update mnist example

--- src/recurrent/recurrent/rnn_train.py
+++ src/recurrent/recurrent/rnn_train.py
@@ -36,39 +36,46 @@ from tensorflow.keras import layers
 ALPHASIZE = config.ALPHASIZE
 INTERNALSIZE = config.INTERNALSIZE
 NLAYERS = config.NLAYERS
-learning_rate = 0.001
-dropout_pkeep = 0.8
-SHUFFLE_BUFFER_SIZE = 50
+LEARNING_RATE = 0.001
+DROPOUT_PKEEP = 0.8
 SEQLEN = 100
 BATCHSIZE = 100
 DISPLAY_FREQ = 50
 _50_BATCHES = DISPLAY_FREQ * BATCHSIZE * SEQLEN
 VALI_SEQLEN = 1 * 1024
-logging.info(
-    "Sequence length for validation = %d. State will be wrong at the start of each sequence.",
-    VALI_SEQLEN
-)
+encoder_vocab = 1000
+decoder_vocab = 2000
+
+logging.debug("%r", "ALPHASIZE = {}".format(ALPHASIZE))
+logging.debug("%r", "INTERNALSIZE = {}".format(INTERNALSIZE))
+logging.debug("%r", "NLAYERS = {}".format(NLAYERS))
+logging.debug("%r", "LEARNING_RATE = {}".format(LEARNING_RATE))
+logging.debug("%r", "DROPOUT_PKEEP = {}".format(DROPOUT_PKEEP))
+logging.debug("%r", "SEQLEN = {}".format(SEQLEN))
+logging.debug("%r", "BATCHSIZE = {}".format(BATCHSIZE))
+logging.debug("%r", "DISPLAY_FREQ = {}".format(DISPLAY_FREQ))
+logging.debug("%r", "_50_BATCHES = {}".format(_50_BATCHES))
+logging.debug("%r", "VALI_SEQLEN = {}".format(VALI_SEQLEN))
 
 data_dir = config.data_dir
 models_dir = config.models_dir
 training_glob_pattern = config.training_glob_pattern
 checkpoints_dir = config.checkpoints_dir
 
-batch_size = 64
-input_dim = 28
-units = 64
-output_size = 10
-encoder_vocab = 1000
-decoder_vocab = 2000
-
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=config.logging_dir, histogram_freq=1)
+tensorboard_callback = tf.keras.callbacks.TensorBoard()
 
 
 def build_bidirectional_model():
     model = tf.keras.Sequential()
 
-    model.add(layers.Bidirectional(layers.LSTM(64, return_sequences=True),
-                                   input_shape=(5, 10)))
+    model.add(
+        layers.Bidirectional(
+            layers.LSTM(
+                64,
+                return_sequences=True
+            ), input_shape=(5, 10)
+        )
+    )
     model.add(layers.Bidirectional(layers.LSTM(32)))
     model.add(layers.Dense(10, activation='softmax'))
     return model
@@ -109,16 +116,16 @@ def build_stateful_model():
     return model
 
 
-def Build_simple_model():
+def build_simple_model():
     # Add an Embedding layer expecting input vocab of size 1000, and
     # output embedding dimension of size 64.
     # Add a LSTM layer with 128 internal units.
     # Add a Dense layer with 10 units and softmax activation.
 
     model = tf.keras.Sequential()
-    model.add(layers.Embedding(input_dim=1000, output_dim=64))
-    model.add(layers.LSTM(128))
-    model.add(layers.Dense(10, activation='softmax'))
+    model.add(layers.Embedding(input_dim=ALPHASIZE, output_dim=INTERNALSIZE))
+    model.add(layers.LSTM(NLAYERS))
+    model.add(layers.Dense(ALPHASIZE, activation='softmax'))
     return model
 
 
@@ -132,7 +139,7 @@ def load_mnist():
     logging.debug("%r", "type(train_dataset) = {}".format(type(train_dataset)))
     logging.debug("%r", "type(test_dataset) = {}".format(type(test_dataset)))
 
-    train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCHSIZE)
+    train_dataset = train_dataset.batch(BATCHSIZE)
     test_dataset = test_dataset.batch(BATCHSIZE)
     return train_dataset, test_dataset
 
@@ -140,7 +147,7 @@ def load_mnist():
 def main():
     train_dataset, test_dataset = load_mnist()
 
-    model = Build_simple_model()
+    model = build_simple_model()
     # model = build_stateful_model()
     # model = build_internal_state_model()
     # model = build_bidirectional_model()
