a9b62f54a571a0c1b37ac7fd1862332da2d05704
Tom Wilson <tom.andrew.wilson@gmail.com>
2019-11-30 16:43:11 -0500

    make paths relative to script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,25 +1,39 @@
-cd src/grapheme2phoneme
+#!/bin/bash
+parent_path=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")/.."
+  pwd -P
+)
+
+cd "$parent_path"
+pwd
+ls
+
+cd $parent_path/src/grapheme2phoneme
+pwd
+ls
 python setup.py develop
 python -m pip install -r requirements.txt
-cd ..
 
-cd src/music-examples
+cd $parent_path/src/music-examples
+pwd
+ls
 python setup.py develop
 python -m pip install -r requirements.txt
-cd ..
 
-cd src/phoneme2grapheme
+cd $parent_path/src/phoneme2grapheme
+pwd
+ls
 python setup.py develop
 python -m pip install -r requirements.txt
-cd ..
 
-cd src/recurrent
+cd $parent_path/src/recurrent
+pwd
+ls
 python setup.py develop
 python -m pip install -r requirements.txt
-cd ..
 
-cd src/scrape-lyrics
+cd $parent_path/src/scrape-lyrics
+pwd
+ls
 python setup.py develop
 python -m pip install -r requirements.txt
-cd ..
-
