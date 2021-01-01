3609d348e307c463431fff59b5655d9c2fe38b94
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-01-01 16:43:57 -0500

    add test

--- tests/test_python_consumer/test_grapheme2phoneme.py
+++ tests/test_python_consumer/test_grapheme2phoneme.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 from python_consumer import consumer
 
@@ -6,8 +8,8 @@ def test_smoke():
     print("is anything on fire?")
 
 
-def test_reduce_to_string(logger):
-    logger.info("test_reduce_to_string")
+def test_reduce_to_string():
+    logging.info("test_reduce_to_string")
     input = [['OW1'],
              ['L', ['AO1', 'R'], 'D'],
              ['AY1'],
@@ -22,8 +24,8 @@ def test_reduce_to_string(logger):
     assert output == expected_output
 
 
-def test_grapheme2phoneme(logger):
-    logger.info("test_grapheme2phoneme")
+def test_grapheme2phoneme():
+    logging.info("test_grapheme2phoneme")
     input = "Words are flowing out like endless rain into a paper cup"
     output = consumer.graphemes2phonemes(input)
     expected_output = [
@@ -41,26 +43,10 @@ def test_grapheme2phoneme(logger):
     assert output == expected_output
 
 
-def test_create_connection_channel(logger):
-    logger.info("test_create_connection_channel")
-    channel = consumer.create_connection_channel()
-
-
-def test_route_callback(logger):
-    logger.info("test_route_callback")
-    pass
-
-
-def test_process_payload(logger):
-    logger.info("test_process_payload")
-    pass
-
-
-def test_callback(logger):
-    logger.info("test_callback")
-    pass
-
-
-def test_main(logger):
-    logger.info("main")
-    pass
+def test_reduce_to_string():
+    logging.info("test_reduce_to_string")
+    input = "Words are flowing out like endless rain into a paper cup"
+    output = consumer.graphemes2phonemes(input)
+    output = consumer.reduce_to_string(output)
+    expected_output = """W ER1 D Z AA1 R F L OW1 IH0 NG AW1 T L AY1 K EH1 N D L AH0 S R EY1 N IH0 N T UW1 AH0 P EY1 P ER0 K AH1 P"""
+    assert output == expected_output
