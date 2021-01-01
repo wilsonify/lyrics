4ba187ec15266f76ecded789d1b37872b656e059
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-01-01 14:46:41 -0500

    add __version__

--- src/tensorflow-consumer/tensorflow_consumer/__init__.py
+++ src/tensorflow-consumer/tensorflow_consumer/__init__.py
@@ -1,3 +1,8 @@
+""" tensorflow consumer, consume messages from amqp and execute tensorflow code """
+
+__version__ = '0.1.0'
+
+
 import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
