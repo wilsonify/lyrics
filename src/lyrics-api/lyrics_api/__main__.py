2a2ced831402a65b5b2ac20d107a7955a603e256
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-01-01 13:34:27 -0500

    setup grapheme and phoneme endpoints

--- src/lyrics-api/lyrics_api/__main__.py
+++ src/lyrics-api/lyrics_api/__main__.py
@@ -1,9 +1,8 @@
 """
-Import FastAPI.
+REST api using FastAPI.
+
 Create an app instance.
-Write a path operation decorator (like @app.get("/")).
-Write a path operation function (like def root(): ... above).
-Run the development server (like uvicorn main:app --reload).
+Run the development server
 
 With FastAPI, by using short, intuitive and standard Python type declarations, you get:
 
@@ -18,125 +17,47 @@ That's probably the main visible advantage of FastAPI compared to alternative fr
 (apart from the raw performance).
 """
 
-from enum import Enum
 from typing import Optional
+
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-import uvicorn
+from pydantic import Field
 
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+class Phoneme(BaseModel):
+    name: str = Field(example="Phoneme")
+    description: Optional[str] = Field(
+        example="""any of the perceptually distinct units of sound in a specified language that distinguish one word from another,
+         for example p, b, d, and t in the English words pad, pat, bad, and bat."""
+    )
+    text: str = Field(example="Y EH1 S T ER0 D EY2 AO1 L M AY1 T R AH1 B AH0 L Z S IY1 M D S OW1 F AA1 R AH0 W EY1")
 
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+class Grapheme(BaseModel):
+    name: str = Field(example="Grapheme")
+    description: Optional[str] = Field(example="grapheme is the smallest functional unit of a writing system")
+    text: str = Field(example="yesterday all my troubles seemed so far away")
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-@app.get("/models2/{model_name}")
-async def get_model2(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-
-@app.get("/items2/")
-async def read_item2(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
-
-
-@app.get("/items3/{item_id}")
-async def read_item3(item_id: str, q: Optional[str] = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-        user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+@app.post("/grapheme2phoneme", response_model=Phoneme)
+async def create_item(item: Grapheme):
     return item
 
 
-@app.get("/items2/{item_id}")
-async def read_user_item2(
-        item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+@app.post("/phoneme2grapheme", response_model=Grapheme)
+async def create_item(item: Phoneme):
     return item
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
-
-@app.put("/items2/{item_id}")
-async def create_item2(item_id: int, item: Item, q: Optional[str] = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
 
 
 if __name__ == "__main__":
-    uvicorn.run("__main__:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run(
+        app="__main__:app",
+        host="127.0.0.1",
+        port=8000,
+        log_level="info",
+        reload_dirs=True
+    )
