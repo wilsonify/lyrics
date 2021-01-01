333c8333666ea58f15a9b9e2621482a0fe628785
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-01-01 11:58:16 -0500

    black formatting

--- src/scrape-lyrics/scrapelyrics/geniusLyrics.py
+++ src/scrape-lyrics/scrapelyrics/geniusLyrics.py
@@ -4,8 +4,10 @@ import json
 import bs4
 import sys
 
-token = '2PtHXnbMH-EAIrKUGitGgN2Edg1lmsLwjvU0jDaETseU8vCPdnxhV0AyslIW_vxD'
-user_agent_string = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0"
+token = "2PtHXnbMH-EAIrKUGitGgN2Edg1lmsLwjvU0jDaETseU8vCPdnxhV0AyslIW_vxD"
+user_agent_string = (
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0"
+)
 
 
 def genius_get_artists(artist_name):
@@ -14,7 +16,11 @@ def genius_get_artists(artist_name):
     returns the top artist in the search results for that string.
 
     """
-    URL = 'https://api.genius.com/search?q=' + artist_name.replace(' ', '%20') + '&per_page=10'
+    URL = (
+            "https://api.genius.com/search?q="
+            + artist_name.replace(" ", "%20")
+            + "&per_page=10"
+    )
     request = urllib.request.Request(URL)
     request.add_header("Authorization", "Bearer " + token)
     request.add_header("User-Agent", user_agent_string)  # otherwise 403 returned
@@ -22,7 +28,7 @@ def genius_get_artists(artist_name):
     raw = response.read()
     json_obj = json.loads(raw)
     for hit in json_obj["response"]["hits"]:
-        primary = hit['result']['primary_artist']
+        primary = hit["result"]["primary_artist"]
         return (primary["id"], primary["name"])
 
 
@@ -32,25 +38,25 @@ def genius_get_songs(artist_id):
     """
     artist_id = str(artist_id)
     result = []
-    baseURL = 'https://api.genius.com/artists/' + artist_id + '/songs'
+    baseURL = "https://api.genius.com/artists/" + artist_id + "/songs"
     i = 1
     while i < 15:
-        URL = baseURL + '?per_page=50&page=' + str(i)
+        URL = baseURL + "?per_page=50&page=" + str(i)
         request = urllib.request.Request(URL)
         request.add_header("Authorization", "Bearer " + token)
         request.add_header("User-Agent", user_agent_string)  # otherwise 403 returned
-        print('requesting page ', i)
+        print("requesting page ", i)
         response = urllib.request.urlopen(request, timeout=4)
         raw = response.read()
         json_obj = json.loads(raw)
-        for song in json_obj['response']['songs']:
-            result.append((song['title'], song['url']))
+        for song in json_obj["response"]["songs"]:
+            result.append((song["title"], song["url"]))
         next_i = json_obj["response"]["next_page"]
-        print('next page = ', next_i)
+        print("next page = ", next_i)
         if next_i:
             i += 1
         else:
-            print('done ', len(result), 'songs found')
+            print("done ", len(result), "songs found")
             break
     return result
 
@@ -60,9 +66,9 @@ def genius_get_lyrics(song_url):
     given a URL of a song, scrape the lyrics and return them as a unicode string
     """
     URL = str(song_url)
-    response = requests.get(URL, headers={'User-Agent': user_agent_string})
+    response = requests.get(URL, headers={"User-Agent": user_agent_string})
     soup = bs4.BeautifulSoup(response.text, "lxml")
-    return soup.find('lyrics').text.strip()
+    return soup.find("lyrics").text.strip()
 
 
 def genius_get_all_lyrics_for_artist(artist):
@@ -71,7 +77,7 @@ def genius_get_all_lyrics_for_artist(artist):
     """
     art = genius_get_artists(artist)
     song_list = genius_get_songs(art[0])
-    lyricFile = open('/home/thom/projects/Lyrics/' + art[1] + 'Lyrics.txt', 'wb')
+    lyricFile = open("/home/thom/projects/Lyrics/" + art[1] + "Lyrics.txt", "wb")
     count = 1.0
     total = float(len(song_list))
     print("get lyrics for artist " + art[1])
@@ -81,17 +87,17 @@ def genius_get_all_lyrics_for_artist(artist):
             print(progress, "%")
         surl = song[1]
         l = "\nTitle:" + song[0] + "\n" + genius_get_lyrics(surl)
-        lyricFile.write(l.encode('utf-8', 'ignore'))
+        lyricFile.write(l.encode("utf-8", "ignore"))
         count += 1
     lyricFile.close()
 
 
 def main():
-    desired = ['led zeppelin', 'pink floyd']
+    desired = ["led zeppelin", "pink floyd"]
     for a in desired:
         print("compile songs for artist: " + a)
         genius_get_all_lyrics_for_artist(a)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
