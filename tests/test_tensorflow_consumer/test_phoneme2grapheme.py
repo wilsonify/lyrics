52547df3a06b4160f02867e95490d1f2f26f5ad0
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-01-10 08:50:23 -0500

    test dataset loading

--- tests/test_tensorflow_consumer/test_phoneme2grapheme.py
+++ tests/test_tensorflow_consumer/test_phoneme2grapheme.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import pytest
+from tensorflow_consumer.config import DATA_DIR
 from tensorflow_consumer.translation import nmt
 from tensorflow_consumer.translation import phoneme2grapheme
 
@@ -40,4 +41,38 @@ def test_create_dataset():
         path=os.path.join(data_dir, "spa-eng", "spa-sample.txt"),
         num_examples=5
     )
-    assert type(output) == zip
+    assert [_ for _ in output] == [
+        ('<start> go . <end>', '<start> go . <end>', '<start> go . <end>', '<start> go . <end>', '<start> hi . <end>'),
+        ('<start> ve . <end>', '<start> vete . <end>', '<start> vaya . <end>', '<start> vayase . <end>',
+         '<start> hola . <end>')
+    ]
+
+
+def test_create_dataset_phone():
+    output = nmt.create_dataset(
+        path=os.path.join(data_dir, "phonemes", "grapheme2phoneme-sample.txt"),
+        num_examples=5
+    )
+    assert [_ for _ in output] == [
+        ('<start> title set fire to that lot ! speech <end>',
+         '<start> rodney burke i ve got one card here and it s on the same subject , '
+         'eh , but this one says , we think the show is great and we dig the beatles '
+         'the most , but we still haven t heard a word from ringo yet . <end>',
+         '<start> ringo arf ! arf ! arf ! arf ! <end>',
+         '<start> rodney and how about him singing ? well , what will you sing for us '
+         ', ringo ? will you say a few words ? <end>',
+         '<start> ringo hello , there , kiddies . i d like to sing a song for you '
+         'today called matchbox . there you go <end>'),
+        ('<start> t ay t ah l s eh t f ay er t uw dh ae t l aa t s p iy ch <end>',
+         '<start> r aa d n iy b er k g aa t w ah n k aa r d hh iy r ah n d ih t s aa '
+         'n dh ah s ey m s ah b jh eh k t eh b ah t dh ih s w ah n s eh z w iy th ih '
+         'ng k dh ah sh ow ih z g r ey t ah n d w iy d ih g dh ah b iy t ah l z dh ah '
+         'm ow s t b ah t w iy s t ih l aeavehnt hh er d ah w er d f r ah m r iy ng g '
+         'ow y eh t <end>',
+         '<start> r iy ng g ow ahrf ahrf ahrf ahrf <end>',
+         '<start> r aa d n iy ah n d hh aw ah b aw t hh ih m s ih ng ih ng w eh l w '
+         'ah t w ih l y uw s ih ng f ao r ah s r iy ng g ow w ih l y uw s ey ah f y '
+         'uw w er d z <end>',
+         '<start> r iy ng g ow hh ah l ow dh eh r k ih d iy z ih d l ay k t uw s ih '
+         'ng ah s ao ng f ao r y uw t ah d ey k ao l d m ae ch b aa k s dh eh r y uw '
+         'g ow <end>')]
