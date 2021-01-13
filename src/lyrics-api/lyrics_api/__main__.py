22f4ffe2a72976fb5c47efbfc66839d06eda7ae5
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-01-13 08:05:54 -0500

    add namespace

--- src/lyrics-api/lyrics_api/__main__.py
+++ src/lyrics-api/lyrics_api/__main__.py
@@ -3,9 +3,10 @@ Run FastAPI behind an ASGI server
 """
 import uvicorn
 
+
 if __name__ == "__main__":
     uvicorn.run(
-        app="controller:app",
+        app="lyrics_api.controller:app",
         host="127.0.0.1",
         port=8000,
         log_level="info",
