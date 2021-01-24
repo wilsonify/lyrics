33497d06e1c92b11cb0699270cfaf379192747ce
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-01-23 19:19:34 -0500

    turn on logging

--- src/lyrics-api/lyrics_api/__main__.py
+++ src/lyrics-api/lyrics_api/__main__.py
@@ -1,12 +1,25 @@
 """
 Run FastAPI behind an ASGI server
 """
+import logging
+from logging.config import dictConfig
+
 import uvicorn
 
 if __name__ == "__main__":
+    dictConfig({
+        "version": 1,
+        "formatters": {
+            "simple": {
+                "format": """%(asctime)s | %(name)-12s | %(levelname)-8s | %(message)s"""
+            }
+        },
+        "handlers": {"console": {"class": "logging.StreamHandler", "formatter": "simple"}},
+        "root": {"handlers": ["console"], "level": logging.DEBUG},
+    })
     uvicorn.run(
         app="lyrics_api.controller:app",
-        host="127.0.0.1",
+        host="localhost",
         port=8000,
         log_level="info",
         reload_dirs=True
